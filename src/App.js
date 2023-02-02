b5fc69a379e2b28c6d2284dad3899eacfce0f896
kire21 <kire.zdraveski@gmail.com>
2023-02-02 11:09:06 +0100

    created edit functions

--- src/App.js
+++ src/App.js
@@ -15,12 +15,13 @@ const App = () => {
       name: todo,
       id: Math.floor(Math.random() * 1000),
       completed: false,
+      isEdit: false,
     };
 
     if (todo.length <= 2) return;
 
     setTodos((prevState) => [...prevState, newTodo]);
-    // setInputValue("");
+    setInputValue("");
   };
 
   const handleChange = (e) => {
@@ -59,12 +60,12 @@ const App = () => {
         return {
           ...todo,
           name: inputValue,
+          isEdit: !todo.isEdit,
         };
       }
       return todo;
     });
     setTodos(filterTodo);
-    // setInputValue("");
   };
 
   return (
@@ -83,6 +84,7 @@ const App = () => {
           placeholder="Enter todo..."
           required
         />
+
         <button
           onClick={() => createTodo(inputValue)}
           className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
@@ -96,7 +98,7 @@ const App = () => {
             className={`${
               todo.completed ? "border-green-400" : " border-blue-400"
             } relative w-2/5 mx-auto my-5 p-4 border-2 rounded-md`}>
-            <h2>{todo.name}</h2>
+            <h2>{todo.name || inputValue}</h2>
             <button
               onClick={() => deleteTodo(todo.id)}
               className="absolute right-2.5 bottom-2.5 bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800 ">
@@ -107,11 +109,19 @@ const App = () => {
               className="absolute right-16 bottom-2.5 bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 ">
               <MdCheckCircleOutline size={18} color="white" />
             </button>
-            <button
-              onClick={() => editTodo(todo.name, todo.id)}
-              className="absolute right-28 mr-2 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ">
-              <MdOutlineModeEditOutline size={18} color="white" />
-            </button>
+            {!todo.isEdit ? (
+              <button
+                onClick={() => editTodo(todo.name, todo.id)}
+                className="absolute right-28 mr-2 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ">
+                <MdOutlineModeEditOutline size={18} color="white" />
+              </button>
+            ) : (
+              <button
+                onClick={() => editTodo(todo.name, todo.id)}
+                className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                Save Changes
+              </button>
+            )}
           </div>
         );
       })}
